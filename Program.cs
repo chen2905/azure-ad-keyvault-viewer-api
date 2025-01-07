1340b874f1307b4982f2881e9bd12bd21aefb9c7
ChenGao <maxlin2905@gmail.com>
2025-01-07 14:07:55 +1100

    80

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ builder.WebHost.ConfigureKestrel(options =>
     });
 });
 
+// Configure the application to use specific URLs
+builder.WebHost.UseUrls("http://*:80");
+
 // Add services to the container
 builder.Services.AddCors(options =>
 {
